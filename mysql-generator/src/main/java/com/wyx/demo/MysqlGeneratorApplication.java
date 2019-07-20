aa9ced40a7be565abac4f3be7eba9e40e6aa0bf2
wangyuxiang985 <wangyuxiang985@gmail.com>
2019-07-20 19:55:35 +0800

    实现逆向工程demo

---
+++ mysql-generator/src/main/java/com/wyx/demo/MysqlGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.wyx.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlGeneratorApplication.class, args);
+    }
+
+}
